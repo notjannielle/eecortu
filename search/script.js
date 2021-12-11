6be569a85aafedf5acd65eac4b55b7918cb76338
notjannielle <94787897+notjannielle@users.noreply.github.com>
2021-12-12 02:07:24 +0800

    Add files via upload

---
+++ search/script.js
@@ -0,0 +1,101 @@
+angular.module('myApp', []).
+controller('controller', ['$scope', controller]);
+
+function controller($scope) {
+  $scope.data = [
+  {
+    "image": "http://vignette2.wikia.nocookie.net/streetfighter/images/6/65/Character_Select_Ryu_by_UdonCrew.jpg/revision/latest?cb=20091113143945",
+    "id": "1111111",
+    "lname": "Tominio",
+    "mname": "Azucena",
+    "fname": "Jannielle",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "O+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "PROFESSIONAL",
+    "past": "OVERSPEEDING",
+    "unpaid": "NONE" },
+
+  {
+    "image": "http://orig13.deviantart.net/4580/f/2008/073/1/5/character_select__ken_by_udoncrew.jpg",
+    "id": "2222222",
+    "lname": "Navarro",
+    "mname": "Tominio",
+    "fname": "Raczy",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "O+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "PROFESSIONAL",
+
+    "past": "OVERSPEEDING",
+    "unpaid": "NONE" },
+
+  {
+    "image": "http://vignette2.wikia.nocookie.net/streetfighter/images/8/82/Character_Select_Akuma_by_UdonCrew.jpg/revision/latest?cb=20091113012042",
+    "id": "3333333",
+    "lname": "Khan",
+    "mname": "Abdul",
+    "fname": "Al",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "O+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "PROFESSIONAL",
+
+    "past": "OVERSPEEDING",
+    "unpaid": "NONE" },
+
+  {
+    "image": "http://vignette2.wikia.nocookie.net/streetfighter/images/0/03/Character_Select_Chun_Li_by_UdonCrew.jpg/revision/latest?cb=20091113143916",
+    "id": "4444444",
+    "lname": "Sy",
+    "mname": "N/A",
+    "fname": "Henry",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "AB+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "NON-PROFESSIONAL",
+    "past": "LIQUOR BAN",
+    "unpaid": "LIQUOR BAN" },
+
+  {
+    "image": "http://vignette4.wikia.nocookie.net/streetfighter/images/d/d9/Character_Select_Sagat_by_UdonCrew.jpg/revision/latest?cb=20091113010442",
+    "id": "5555555",
+    "lname": "Zuckerberg",
+    "mname": "",
+    "fname": "Mark",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "O+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "PROFESSIONAL",
+
+    "past": "RECKLESS DRIVING",
+    "unpaid": "RECKLESS DRIVING" },
+
+  {
+    "image": "https://s-media-cache-ak0.pinimg.com/736x/ce/9d/29/ce9d293cd1b7cb2c302d9a179cc9b4de.jpg",
+    "id": "6666666",
+    "lname": "Bezos",
+    "mname": "Azucena",
+    "fname": "Jeff",
+    "contact": "0952-123-4222",
+    "emergency": "7234-1234",
+    "blood": "AB+",
+    "gender": "M",
+    "address": "131 Pantaleon St, Mandaluyong City",
+    "status": "PROFESSIONAL",
+
+    "past": "NONE",
+    "unpaid": "OVERSPEEDING" }];
+
+
+}
